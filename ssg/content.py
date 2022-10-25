31fc975182f2b54f9368c0ae39d485c74d63d86b
Jesus Caceres <jesus.caceres@southworks.com>
2022-10-25 11:37:21 -0300

    Fix v5

--- ssg/content.py
+++ ssg/content.py
@@ -5,8 +5,8 @@ from collections.abc import Mapping
 
 
 class Content(Mapping):
-    __delimeter = "^(?:-|\+){3}\s*$"
-    __regex = re.compile(__delimeter, re.MULTILINE)
+    __delimiter = "^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimiter, re.MULTILINE)
 
     @classmethod
     def load(cls, string):
@@ -27,11 +27,11 @@ class Content(Mapping):
         return self.data["type"] if "type" in self.data else None
 
     @type.setter
-    def type(self, t):
-        self.data["type"] = t
+    def type(self, type):
+        self.data["type"] = type
 
-    def __getitem__(self, __key):
-        return self.data[__key]
+    def __getitem__(self, key):
+        return self.data[key]
 
     def __iter__(self):
         self.data.__iter__()
@@ -42,6 +42,6 @@ class Content(Mapping):
     def __repr__(self):
         data = {}
         for key, value in self.data.items():
-            if key is not "content":
+            if key != "content":
                 data[key] = value
         return str(data)
