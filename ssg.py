a013377be9c828b8859e38b98bdcd5c0830d39f2
Jesus Caceres <jesus.caceres@southworks.com>
2022-10-25 11:18:20 -0300

    The Parser class

--- ssg.py
+++ ssg.py
@@ -1,9 +1,14 @@
 import typer
+import ssg.parsers
 from ssg.site import Site
 
 
 def main(source="content", dest="dist"):
-    config = {source: source, dest: dest}
+    config = {
+        "source": source,
+        "dest": dest,
+        "parsers": [ssg.parsers.ResourceParser()]
+    }
     s = Site(**config).build()
 
 
