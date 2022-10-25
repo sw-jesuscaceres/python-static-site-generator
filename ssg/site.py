38e22685d7037661ff282eb381856337c29a6a63
Jesus Caceres <jesus.caceres@southworks.com>
2022-10-25 10:57:42 -0300

    The Site Class

---
+++ ssg/site.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+
+class Site:
+    def __init__(self, source, dest) -> None:
+        self.source: Path = Path(source)
+        self.dest: Path = Path(dest)
+
+    def create_dir(self, path: Path):
+        directory: Path = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
+
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
+        for path in self.source.rglob("*"):
+            if path.is_dir():
+                self.create_dir(path)
+    
